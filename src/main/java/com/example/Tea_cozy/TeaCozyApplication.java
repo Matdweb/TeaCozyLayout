1bd7926ab3a737050e298d57244927a7eebda08a
Mat_dweb <110640534+Matdweb@users.noreply.github.com>
2024-06-16 20:06:47 -0600

    First commit

---
+++ src/main/java/com/example/Tea_cozy/TeaCozyApplication.java
@@ -0,0 +1,13 @@
+package com.example.Tea_cozy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeaCozyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeaCozyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Tea_cozy/TeaCozyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Tea_cozy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeaCozyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
